12d296afb83aed21e7c9cb97b6ee22147b67f9c0
Usama Ahmed <maxpaynestory@gmail.com>
2024-02-12 12:05:21 +0500

    added prefix and eloquent model

---
+++ app/Models/EventCalendarSetting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventCalendarSetting extends Model
+{
+    use HasFactory;
+}
